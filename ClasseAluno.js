61c3073fe10ac11e4fa3febf1ad195d7806f9c1e
Tamirys Hariadny Camargo Couto <tamirys.couto@escola.pr.gov.br>
2026-04-15 13:02:29 +0000

    commit

--- ClasseAluno.js
+++ ClasseAluno.js
@@ -1,18 +1,50 @@
-class Aluno{
-    constructor(nome,matricula,notas){
-        this.nome = this.nome;
+class Aluno {
+    constructor(nome, matricula, notas) {
+        this.nome = nome;
         this.matricula = matricula;
         this.notas = notas;
     }
 
-    calculaMedia(){
+    calculaMedia() {
         let soma = 0;
-        for(let i= 0; i < this.notas.length; i++){
+        for (let i = 0; i < this.notas.length; i++) {
             soma += this.notas[i];
-
         }
         return soma / this.notas.length;
     }
+
+    obterStatus() {
+        const media = this.calculaMedia();
+        if (media >= 7) {
+            return "Aprovado";
+        } else if (media >= 5) {
+            return "Recuperação";
+        } else {
+            return "Reprovado";
+        }
+    }
+    exibirRelatorio(){
+        console.log(`=============================================`);
+        console.log(`Relatorio do aluno ${this.nome}`);
+        console.log(`Matricula ${this.matricula}`);
+        console.log(`Media Final ${this.calculaMedia().toFixed(2)}`);
+        console.log(`Status ${this.obterStatus()}`);
+        console.log(`=============================================`);
+        
+
+    }
 }
-   let aluno1 = new Aluno("alice", 5050,[8.0,7.3,9.0]);
-   console.log(aluno1.calculaMedia());
+
+
+
+let turma = [
+    new Aluno(`Alice Ença`, `CGM5050`, [8.0, 7.3, 9.0]),
+    new Aluno(`Jacinto Pontes`, `CGM5051`, [2.5,7.8,4.2]),
+    new Aluno(`Oscar Alho`, `CGM5052`,[5.0,8.0,9.0]),
+    new Aluno(`Amandão Caminhoneria`,`CGM5053` ,[5.9,2.9,1.0]),
+]
+
+
+for (let i = 0; i < turma.length; i++ ){
+    turma[i].exibirRelatorio();
+}
